87afd2f0a3168232031bbaffabc633039c4ff20c
Noro Jurík <noro.jurik@gmail.com>
2020-04-13 14:14:28 +0200

    Update

--- src/model/formula/Formula.PredicateAtom.js
+++ src/model/formula/Formula.PredicateAtom.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Formula from "./Formula";
 
 class PredicateAtom extends Formula {
@@ -15,30 +14,4 @@ class PredicateAtom extends Formula {
 
 }
 
-=======
-import Formula from "./Formula";
-
-class PredicateAtom extends Formula {
-
-  constructor(name, terms = []) {
-    super();
-    this.name = name;
-    this.terms = terms;
-  }
-
-  toString() {
-    let res = this.name + '(';
-    for (let i = 0; i < this.terms.length; i++) {
-      if (i > 0) {
-        res += ', ';
-      }
-      res += this.terms[i].toString();
-    }
-    res += ')';
-    return res;
-  }
-
-}
-
->>>>>>> b48bca53b8673ff637bc96026739d5b489709c96
 export default PredicateAtom;
--- src/model/term/Term.FunctionTerm.js
+++ src/model/term/Term.FunctionTerm.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Term from "./Term";
 
 class FunctionTerm extends Term {
@@ -28,35 +27,4 @@ class FunctionTerm extends Term {
 
 }
 
-=======
-import Term from "./Term";
-
-class FunctionTerm extends Term {
-
-  constructor(name, subts = []) {
-    super(name);
-    this.subts = subts;
-  }
-
-  toString() {
-    return this.name + '(' + this.subts.map(subt => subt.toString()).join(', ') + ')';
-  }
-
-  equals(other) {
-    return other instanceof FunctionTerm &&
-      this.name === other.name &&
-      this.subts.length === other.subts.length &&
-      this.subts.every((subt, i) => subt.equals(other.subts[i]))
-  }
-
-  substitute(map){
-    return new FunctionTerm(
-      this.name,
-      this.subts.map( subt => subt.substitute(map))
-    );
-  }
-
-}
-
->>>>>>> b48bca53b8673ff637bc96026739d5b489709c96
 export default FunctionTerm;
