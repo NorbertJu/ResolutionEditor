658d5fc03a8d7c3a817a1ce1bed2c9cdba2250f8
Noro Jurík <noro.jurik@gmail.com>
2020-05-13 10:36:37 +0200

    Updates

---
+++ precache-manifest.646023e30bad78c03e1f22451ee98f89.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371ef9f397fc7c983be340645aade972",
+    "url": "/ResolutionEditor/index.html"
+  },
+  {
+    "revision": "62d3b721f3ea146f847e",
+    "url": "/ResolutionEditor/static/js/2.14b41f20.chunk.js"
+  },
+  {
+    "revision": "8a29f17fd04560a028f9",
+    "url": "/ResolutionEditor/static/js/main.2ddd39ff.chunk.js"
+  },
+  {
+    "revision": "041ddca62880e408c05e",
+    "url": "/ResolutionEditor/static/js/runtime-main.65f4431d.js"
+  }
+]);
