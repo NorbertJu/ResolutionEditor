48f94b98230aed6f2a707161d20fda7b35533efe
Noro Jurík <noro.jurik@gmail.com>
2020-05-21 17:36:32 +0200

    help component

--- src/components/Help.js
+++ src/components/Help.js
@@ -4,7 +4,7 @@ const Help = () => (
   <div className="mt-2 d-inline-block ">
     <h4>Help</h4>
     <div className="mb-2">
-      <p className="mb-0">Languauge - Declarations of non-logical symbols:</p>
+      <p className="mb-0">Language - Declarations of non-logical symbols:</p>
       <ul className="fs-sm">
         <li >Symbols declarations must be comma-separated</li>
         <li >A constant declaration is just the constant symbol</li>
